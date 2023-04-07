be8b38427e37fc35fb0b5038f912ae0e47ae05c5
Sumesh Murali <sumesh@scrapehero.com>
2023-04-07 21:59:22 +0530

    frontend functionality mostly done

---
+++ src/components/CategoryView.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+// const emit = defineEmits([])
+
+const props = defineProps<{
+  categories: {name: string, totalTimeSpend: number}[]
+}>()
+
+const emit = defineEmits<{
+  (e: 'add-category-btn-clicked'): void,
+  (e: 'add-time-clicked', category: string): void
+}>()
+
+</script>
+
+<template>
+  <div class="flex flex-col w-3/4">
+    <div class="h-fit self-end mr-20">
+        <button
+          class="text-black text-lg bg-green-300 hover:bg-green-800 hover:text-white p-5 rounded-lg pr-10 pl-10"
+          @click="emit('add-category-btn-clicked')"
+        >
+          Add new category
+        </button>
+      </div>
+    
+    
+    <div class="grid grid-cols-4">
+      <h1 class="text-white text-xl" v-if="!props.categories">Start by adding a new category</h1>
+      <button 
+      v-else v-for="category in props.categories" :key="category.totalTimeSpend"
+      class="block max-w-sm p-6 m-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      @click="emit('add-time-clicked', category.name)"
+      >
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{category.name}}</h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">{{ category.totalTimeSpend }}</p>
+      </button>
+    </div>
+    
+  </div>
+</template>
---
+++ src/components/RecentTasks.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+
+const props = defineProps<{
+  recentTasks: {name: string, type: string, duration: string, added: number}[]
+}>()
+
+const sortedTasks = computed(() => {
+  return [...props.recentTasks].sort((a,b)=> b.added-a.added)
+})
+
+</script>
+
+<template>
+  <!-- Side menu -->
+  <div class="relative h-screen grow-1 space-y-10 overflow-x-auto self-center">
+    <h2
+      class="m-4 text-3xl font-extrabold leading-none tracking-tight text-white md:text-4xl dark:text-white"
+    >
+      Recent Tasks
+    </h2>
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" class="px-4 py-3">Name</th>
+          <th scope="col" class="px-4 py-3">Type</th>
+          <th scope="col" class="px-4 py-3">Time Spend</th>
+          <th scope="col" class="px-4 py-3">Added</th>
+        </tr>
+      </thead>
+      <tbody class="text-xs">
+        <tr v-if="!sortedTasks" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <td colspan="4" class="px-4 py-2">
+          Your recent tasks will appear here
+        </td>
+        </tr>
+        <tr v-else v-for="task in sortedTasks" :key="task.added" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+          <th
+              scope="row"
+              class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              {{task.name}}
+            </th>
+            <td class="px-4 py-2">{{task.type}}</td>
+            <td class="px-4 py-2">{{ task.duration }}</td>
+            <td class="px-4 py-2">{{ task.added }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
